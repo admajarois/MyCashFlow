05535058e7468b00bf9013e8dc19a01cd9219d49
AdmajaRois <rois.admj@gmail.com>
2021-10-23 10:31:41 +0700

    add login and splash screen

--- app/src/main/java/com/example/mycashflow/activities/LoginActivity.java
+++ app/src/main/java/com/example/mycashflow/activities/LoginActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.mycashflow.R;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            this.getSupportActionBar().hide();
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
         setContentView(R.layout.activity_login);
     }
 }
--- app/src/main/java/com/example/mycashflow/activities/SplashActivity.java
+++ app/src/main/java/com/example/mycashflow/activities/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.mycashflow.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.mycashflow.R;
 
@@ -11,6 +13,19 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            this.getSupportActionBar().hide();
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
         setContentView(R.layout.activity_splash_activiry);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 5000);
     }
 }
