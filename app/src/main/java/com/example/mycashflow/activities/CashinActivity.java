038ca34ae1e176e7ace0ecf3b001f7eb1947a6ae
AdmajaRois <rois.admj@gmail.com>
2021-10-24 21:43:20 +0700

    adding cashin and cashout

---
+++ app/src/main/java/com/example/mycashflow/activities/CashinActivity.java
@@ -0,0 +1,58 @@
+package com.example.mycashflow.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+
+import com.example.mycashflow.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class CashinActivity extends AppCompatActivity {
+
+    TextInputLayout textInputLayout;
+    DatePickerDialog datePickerDialog;
+    SimpleDateFormat dateFormat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            this.getSupportActionBar().hide();
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
+        setContentView(R.layout.activity_cashin);
+        textInputLayout = findViewById(R.id.tanggal_cashin);
+
+        dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.forLanguageTag("INDONESIA"));
+
+        textInputLayout.getEditText().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateDialog();
+            }
+        });
+    }
+
+    private void showDateDialog() {
+        Calendar calendar = Calendar.getInstance();
+
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, month, dayOfMonth);
+                textInputLayout.getEditText().setText(dateFormat.format(newDate.getTime()));
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.show();
+    }
+}
--- app/src/main/java/com/example/mycashflow/activities/LoginActivity.java
+++ app/src/main/java/com/example/mycashflow/activities/LoginActivity.java
@@ -2,8 +2,12 @@ package com.example.mycashflow.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.mycashflow.MainActivity;
 import com.example.mycashflow.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -17,5 +21,11 @@ public class LoginActivity extends AppCompatActivity {
             System.out.println(e);
         }
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void handleLogin(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
